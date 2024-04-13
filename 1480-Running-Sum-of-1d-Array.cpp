285428ecff1d977197615e17f679a6c0c38f49a9
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-13 13:51:30 +0200

    [1480] [Time Beats: 100.00%] [Memory Beats: 14.87%] - LeetPush

--- 1480-Running-Sum-of-1d-Array.cpp
+++ 1480-Running-Sum-of-1d-Array.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> sum(nums.size(), 0);
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i) sum[i] = nums[i] + sum[i - 1];
-            else sum[i] = nums[i];
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
         }
-        return sum;
+        return nums;
     }
 };
