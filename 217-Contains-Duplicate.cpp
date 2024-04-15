5c6400dbc43f84d2f0ed738f3d2687a35ff22130
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-15 17:43:52 +0200

    [217] [Time Beats: 98.76%] [Memory Beats: 82.30%] - LeetPush

--- 217-Contains-Duplicate.cpp
+++ 217-Contains-Duplicate.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
         sort(nums.begin(), nums.end());
 
         for (int i = 1; i < nums.size(); i++) {
