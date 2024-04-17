5101290e1ff68c50acb4af6cfefb1a1daaad6c28
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-17 17:20:39 +0200

    Update 1472-Design-Browser-History.cpp

--- 1472-Design-Browser-History.cpp
+++ 1472-Design-Browser-History.cpp
@@ -28,10 +28,46 @@ public:
     }
 };
 
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory* obj = new BrowserHistory(homepage);
- * obj->visit(url);
- * string param_2 = obj->back(steps);
- * string param_3 = obj->forward(steps);
- */
+// Solution using stacks
+/*
+#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+class BrowserHistory {
+    stack<string> sites, history;
+    int length;
+public:
+    BrowserHistory(string homepage) {
+        sites.push(homepage);
+    }
+
+    void visit(string url) {
+        IOS
+        while (!history.empty()) history.pop();
+        sites.push(url);
+    }
+
+    string back(int steps) {
+        IOS
+        if (steps > sites.size() - 1) length = sites.size() - 1;
+        else length = steps;
+
+        for (int i = 0; i < length; i++) {
+            history.push(sites.top());
+            sites.pop();
+        }
+        return sites.top();
+    }
+
+    string forward(int steps) {
+        IOS
+        if (steps > history.size()) length = history.size();
+        else length = steps;
+
+        for (int i = 0; i < length; i++) {
+            sites.push(history.top());
+            history.pop();
+        }
+        return sites.top();
+    }
+};
+
+*/
