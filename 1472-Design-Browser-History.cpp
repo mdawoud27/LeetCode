74c3cdb123216809da8fffb0b744a6b90fcae425
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-17 17:11:42 +0200

    [1472] [Time Beats: 90.56%] [Memory Beats: 79.66%] - LeetPush

--- 1472-Design-Browser-History.cpp
+++ 1472-Design-Browser-History.cpp
@@ -1,40 +1,30 @@
 #define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
 class BrowserHistory {
-    stack<string> sites, history;
-    int length;
+    vector<string> history;
+   int idx;
 public:
     BrowserHistory(string homepage) {
-        sites.push(homepage);
+        history.push_back(homepage);
+        idx = 1;
     }
 
     void visit(string url) {
         IOS
-        while (!history.empty()) history.pop();
-        sites.push(url);
+        history.resize(idx);
+        idx++;
+        history.push_back(url);
     }
 
     string back(int steps) {
         IOS
-        if (steps > sites.size() - 1) length = sites.size() - 1;
-        else length = steps;
-
-        for (int i = 0; i < length; i++) {
-            history.push(sites.top());
-            sites.pop();
-        }
-        return sites.top();
+        idx = max(idx - steps, 1);
+        return history[idx - 1];
     }
 
     string forward(int steps) {
         IOS
-        if (steps > history.size()) length = history.size();
-        else length = steps;
-
-        for (int i = 0; i < length; i++) {
-            sites.push(history.top());
-            history.pop();
-        }
-        return sites.top();
+        idx = min(idx + steps, int(history.size()));
+        return history[idx - 1];
     }
 };
 
