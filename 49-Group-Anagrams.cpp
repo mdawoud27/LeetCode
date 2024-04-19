87a971fefdc0ba38cbc6df7327b22044e0146565
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-19 18:27:15 +0200

    [49] [Time Beats: 39.05%] [Memory Beats: 37.49%] - LeetPush

---
+++ 49-Group-Anagrams.cpp
@@ -0,0 +1,20 @@
+#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        IOS 
+        unordered_map<string, vector<string>> mp;
+
+        for (auto str : strs) {
+            string word = str;
+            sort(word.begin(), word.end());
+            mp[word].push_back(str);
+        }
+
+        vector<vector<string>> result;
+        for (auto keyValue : mp)
+            result.push_back(keyValue.second);
+
+        return result;
+    }
+};
