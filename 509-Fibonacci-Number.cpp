b45c6a7517e791990a57a6c7c2e041521b97e7da
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-21 22:45:16 +0200

    [509] [Time Beats: 79.32%] [Memory Beats: 72.76%] - LeetPush

--- 509-Fibonacci-Number.cpp
+++ 509-Fibonacci-Number.cpp
@@ -1,9 +1,17 @@
+#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
 class Solution {
 public:
     int fib(int n) {
+        IOS
         if (n <= 1)
             return n;
 
-        return fib(n - 1) + fib(n - 2);
+        int n1 = 0, n2 = 1, n3;
+        for (int i = 2; i <= n; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
     }
 };
