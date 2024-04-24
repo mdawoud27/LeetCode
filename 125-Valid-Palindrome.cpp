fd73fd3285b334f59ab6d47c77e70ad0782bcbcc
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-24 10:17:35 +0200

    [125] [Time Beats: 100.00%] [Memory Beats: 63.00%] - LeetPush

--- 125-Valid-Palindrome.cpp
+++ 125-Valid-Palindrome.cpp
@@ -6,8 +6,8 @@ public:
         int i = 0, j = s.size() - 1;
 
         while (i < j) {
-            if (!isalnum(s[i])) {i++; continue;}
-            if (!isalnum(s[j])) {j--; continue;}
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
             if (tolower(s[i]) == tolower(s[j])) {
                 i++;
                 j--;
