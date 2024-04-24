cc2741eb840e22719d0f20639eec255d7cd4a35e
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-24 10:11:28 +0200

    [125] [Time Beats: 100.00%] [Memory Beats: 39.09%] - LeetPush

---
+++ 125-Valid-Palindrome.cpp
@@ -0,0 +1,18 @@
+#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        IOS
+        int i = 0, j = s.size() - 1;
+
+        while (i < j) {
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
+            if (tolower(s[i]) == tolower(s[j])) {
+                i++;
+                j--;
+            } else return false;
+        }
+        return true;
+    }
+};
