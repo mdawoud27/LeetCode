1acfb27b49c5650af87e55eb806dab6c2e813035
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-25 07:16:01 +0200

    [88] [Time Beats: 48.36%] [Memory Beats: 6.36%] - LeetPush

---
+++ 88-Merge-Sorted-Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0, j = 0;
+        vector<int> sortedArray;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j])
+                sortedArray.push_back(nums1[i++]);
+            else
+                sortedArray.push_back(nums2[j++]);
+        }
+        while (i < m) sortedArray.push_back(nums1[i++]);
+        while (j < n) sortedArray.push_back(nums2[j++]);
+
+        for (int k = 0; k < m + n; k++)
+            nums1[k] = sortedArray[k];
+    }
+};
