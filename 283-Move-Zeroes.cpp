6e018642765a31de8e74189f7db66e2146fef7da
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-25 06:35:37 +0200

    Update 283-Move-Zeroes.cpp

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // 2 pointers
         int nonZeroIdx = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
@@ -10,3 +11,17 @@ public:
         }
     }
 };
+
+/*
+void moveZeroes(vector<int>& nums) {
+    int noZeroIdx = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[noZeroIdx++] = nums[i];
+        }
+    }
+
+    while (noZeroIdx < nums.size())
+        nums[noZeroIdx++] = 0;
+}
+*/
