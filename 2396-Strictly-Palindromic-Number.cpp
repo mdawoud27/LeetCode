8c01eadaf1e1d722d2317e06ea0e4906c6398186
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-04-27 20:07:00 +0300

    [2396] [Time Beats: 52.44%] [Memory Beats: 6.13%] - LeetPush

---
+++ 2396-Strictly-Palindromic-Number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string convertToBaseNum(int n, int base) {
+        string temp = "";
+
+        while (n > 0) {
+            temp += to_string(n % base);
+            n /= base;
+        }
+        reverse(temp.begin(), temp.end());
+        return temp;
+    }
+
+    bool isPalindrom(string number) {
+        int i = 0, j = number.size();
+
+        while (i < j) {
+            if (number[i] != number[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool isStrictlyPalindromic(int n) {
+        for (int i = 2; i <= n - 2; i++) {
+            string temp = convertToBaseNum(n, i);
+            bool flag = isPalindrom(temp);
+
+            if (!flag) return false;
+        }
+        return true;
+    }
+};
