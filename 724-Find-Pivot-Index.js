6b092e94f5453413073748b7597fec72f6222d6a
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-02 22:51:23 +0300

    [724] [Time Beats: 99.24%] [Memory Beats: 91.94%] - LeetPush

---
+++ 724-Find-Pivot-Index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    let totalSum = nums.reduce((acc, cur) => acc + cur, 0);
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === totalSum - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    return -1;
+};
