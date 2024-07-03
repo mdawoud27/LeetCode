132b79fca694fd02c606cc8da4575df81bd8e37d
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-03 21:00:53 +0300

    [2215] [Time Beats: 15.80%] [Memory Beats: 96.87%] - LeetPush

--- 2215-Find-the-Difference-of-Two-Arrays.js
+++ 2215-Find-the-Difference-of-Two-Arrays.js
@@ -3,24 +3,9 @@
  * @param {number[]} nums2
  * @return {number[][]}
  */
-var findDifference = function(nums1, nums2) {
-    let answer = [];
-  let ans1 = new Set();
-  let ans2 = new Set();
+var findDifference = function (nums1, nums2) {
+    let ans1 = nums1.filter(num => !nums2.includes(num));
+    let ans2 = nums2.filter(num => !nums1.includes(num));
 
-  let set1 = new Set(nums1);
-  let set2 = new Set(nums2);
-
-  for (let num of set1) {
-    if (!set2.has(num)) ans1.add(num);
-  }
-
-  for (let num of set2) {
-    if (!set1.has(num)) ans2.add(num);
-  }
-
-  answer.push([...ans1]);
-  answer.push([...ans2]);
-
-  return answer;
+    return [[...new Set(ans1)], [...new Set(ans2)]];  
 };
