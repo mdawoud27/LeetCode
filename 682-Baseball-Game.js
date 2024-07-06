196cef1b320243e5ab04f079db8eba8856f4b091
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-06 18:01:35 +0300

    [682] [Time Beats: 36.74%] [Memory Beats: 69.95%] - LeetPush

---
+++ 682-Baseball-Game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    let record = [];
+
+    for (let i = 0; i < operations.length; i++) {
+        if (Number(operations[i])) record.push(Number(operations[i]));
+        else if (operations[i] === 'C') record.pop();
+        else if (operations[i] === 'D') record.push(record[record.length - 1] * 2);
+        else if (operations[i] === '+') record.push(record[record.length - 1] + record[record.length - 2]);
+    }
+
+    return record.reduce((acc, curr) => acc + curr, 0);
+};
