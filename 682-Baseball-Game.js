8fc0a3d6c71c40f1039272c432cb94406d9716f4
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-06 18:02:53 +0300

    [682] [Time Beats: 18.10%] [Memory Beats: 54.21%] - LeetPush

--- 682-Baseball-Game.js
+++ 682-Baseball-Game.js
@@ -2,14 +2,18 @@
  * @param {string[]} operations
  * @return {number}
  */
-var calPoints = function(operations) {
+var calPoints = function (operations) {
     let record = [];
 
     for (let i = 0; i < operations.length; i++) {
-        if (Number(operations[i])) record.push(Number(operations[i]));
-        else if (operations[i] === 'C') record.pop();
-        else if (operations[i] === 'D') record.push(record[record.length - 1] * 2);
-        else if (operations[i] === '+') record.push(record[record.length - 1] + record[record.length - 2]);
+        if (Number(operations[i]))
+            record.push(Number(operations[i]));
+        else if (operations[i] === 'C')
+            record.pop();
+        else if (operations[i] === 'D')
+            record.push(record[record.length - 1] * 2);
+        else if (operations[i] === '+')
+            record.push(record[record.length - 1] + record[record.length - 2]);
     }
 
     return record.reduce((acc, curr) => acc + curr, 0);
