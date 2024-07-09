549c2e2ebcfc52e213cf500e19d946ca0f2a2192
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-09 22:11:36 +0300

    [143] [Time Beats: 46.60%] [Memory Beats: 68.51%] - LeetPush

--- 143-Reorder-List.cpp
+++ 143-Reorder-List.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        /* // Recursion Approach
         if (!head || !head->next || !head->next->next)
             return;
 
@@ -23,5 +24,43 @@ public:
         secondLastNode->next = NULL;
 
         reorderList(head->next->next);
+        */
+
+        /*Two Pointer Approach*/
+
+        // step 1. find the middle of the list using slow and fast pointer
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        } // Now slow pointer is at the middle
+
+        // step 2. reverse the second half
+        ListNode *prev = nullptr;
+        ListNode *curr = slow->next;
+        slow->next = nullptr; // break the list into halves
+
+        while (curr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // step 3. merge the 2 halves
+        ListNode *first = head;
+        ListNode *second = prev;
+
+        while (second) {
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
     }
 };
