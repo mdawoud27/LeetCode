f76125746a3b704b1bc022dbb627f17438193968
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-09 20:24:36 +0300

    [21] [Time Beats: 51.02%] [Memory Beats: 43.16%] - LeetPush

--- 21-Merge-Two-Sorted-Lists.cpp
+++ 21-Merge-Two-Sorted-Lists.cpp
@@ -12,23 +12,23 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* mergedList = new ListNode(0);
-        ListNode *ptr1 = list1, *ptr2 = list2, *ptr3 = mergedList;
+        ListNode *ptr = mergedList;
 
-        while (ptr1 && ptr2) {
-            if (ptr1->val <= ptr2->val) {
-                ptr3->next = ptr1;
-                ptr1 = ptr1->next;
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                ptr->next = list1;
+                list1 = list1->next;
             } else {
-                ptr3->next = ptr2;
-                ptr2 = ptr2->next;
+                ptr->next = list2;
+                list2 = list2->next;
             }
-            ptr3 = ptr3->next;
+            ptr = ptr->next;
         }
 
-        if (ptr1)
-            ptr3->next = ptr1;
-        else if (ptr2)
-            ptr3->next = ptr2;
+        if (list1)
+            ptr->next = list1;
+        else if (list2)
+            ptr->next = list2;
 
         return mergedList->next;
     }
