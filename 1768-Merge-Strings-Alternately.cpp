8da32bd72019df91b2124a110ff5ff94a88ef066
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-17 17:22:21 +0300

    [1768] [Time Beats: 45.63%] [Memory Beats: 10.39%] - LeetPush

--- 1768-Merge-Strings-Alternately.cpp
+++ 1768-Merge-Strings-Alternately.cpp
@@ -1,6 +1,8 @@
+#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        IOS
         string mergedStr = "";
         int i = 0, j = 0;
         int minSize = min(word1.size(), word2.size());
@@ -9,18 +11,22 @@ public:
             mergedStr += word1[i];
             mergedStr += word2[j];
             i++, j++;
-        }
+        } // O(n)
 
         while (i < word1.size()) {
             mergedStr += word1[i];
             i++;
-        }
+        } // O(m - n)
 
         while (j < word2.size()) {
             mergedStr += word2[j];
             j++;
-        }
+        } // O(k - n)
 
-        return mergedStr;   
+        // Time Complexity: O(m + k)
+        // Space Complexity: O(m + k) because the final str is a combination of
+        // 2 strings.
+
+        return mergedStr;
     }
 };
