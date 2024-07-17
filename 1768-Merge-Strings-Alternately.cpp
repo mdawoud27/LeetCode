91b37da2efada3211d0032e6071ff9cb0f5d7d24
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-17 17:14:24 +0300

    [1768] [Time Beats: 100.00%] [Memory Beats: 12.73%] - LeetPush

--- 1768-Merge-Strings-Alternately.cpp
+++ 1768-Merge-Strings-Alternately.cpp
@@ -1,39 +1,26 @@
-#define IOS ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        IOS
-        // Two Pointers
         string mergedStr = "";
         int i = 0, j = 0;
-        
-        while (i < word1.size() && j < word2.size()) {
-            mergedStr += word1[i++];
-            mergedStr += word2[j++];
+        int minSize = min(word1.size(), word2.size());
+
+        while (minSize--) {
+            mergedStr += word1[i];
+            mergedStr += word2[j];
+            i++, j++;
         }
 
-        while (i < word1.size()) mergedStr += word1[i++];
-        while (j < word2.size()) mergedStr += word2[j++];
-        
-        return mergedStr;
+        while (i < word1.size()) {
+            mergedStr += word1[i];
+            i++;
+        }
+
+        while (j < word2.size()) {
+            mergedStr += word2[j];
+            j++;
+        }
+
+        return mergedStr;   
     }
 };
-
-/*
-
-string mergeAlternately(string word1, string word2) {
-    IOS
-    string mergedStr = "";
-    int minLength = min(word1.size(), word2.size());
-
-    for (int i = 0; i < minLength; i++) {
-        mergedStr += word1[i];
-        mergedStr += word2[i];
-    }
-
-    mergedStr += word1.substr(minLength);
-    mergedStr += word2.substr(minLength);
-
-    return mergedStr;
-}
-*/
