8a2c425bba746eb106019ba98c9d6970b86bb843
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-19 19:37:22 +0300

    [344] [Time Beats: 97.37%] [Memory Beats: 5.81%] - LeetPush

--- 344-Reverse-String.cpp
+++ 344-Reverse-String.cpp
@@ -9,9 +9,7 @@ public:
         int i = 0, j = s.size() - 1;
 
         while (i < j) {
-            swap(s[i], s[j]);
-            i++;
-            j--;
+            swap(s[i++], s[j--]);
         }
     }
 };
