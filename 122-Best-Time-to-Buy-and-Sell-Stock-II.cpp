8e77defe1df36f07bbf81a9c26fd4ffe80b230b3
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-21 23:02:56 +0300

    Update 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp

--- 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -4,9 +4,8 @@ public:
         int maxProfit = 0;
 
         for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] > prices[i - 1]) {
+            if (prices[i] > prices[i - 1])
                 maxProfit += prices[i] - prices[i - 1];
-            }
         }
         return maxProfit;
     }
