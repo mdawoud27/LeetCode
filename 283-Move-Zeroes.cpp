fb41803a9ca1d8971cd82c441f643278db76346f
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-07-22 18:27:33 +0300

    [283] [Time Beats: 90.58%] [Memory Beats: 13.72%] - LeetPush

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // 2 pointers
-        int nonZeroIdx = 0;
+        int j = 0; // pointer to non-zero elements of the array
+
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[nonZeroIdx], nums[i]);
-                nonZeroIdx++;
+            if (nums[i]) {
+                swap(nums[j], nums[i]);
+                j++;
             }
         }
     }
 };
-
-/*
-void moveZeroes(vector<int>& nums) {
-    int noZeroIdx = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            nums[noZeroIdx++] = nums[i];
-        }
-    }
-
-    while (noZeroIdx < nums.size())
-        nums[noZeroIdx++] = 0;
-}
-*/
