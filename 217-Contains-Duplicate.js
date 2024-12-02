0cfb75df35c0336634787872a5449370850550c3
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-12-02 14:23:10 +0200

    [217] [Time Beats: 20.62%] [Memory Beats: 47.82%] - LeetPush

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    const setNums = new Set(nums);
+    /** 
+    // Approach 1: convert array to set and compare between their size
 
+    const setNums = new Set(nums);
     return nums.length !== setNums.size;
+    */
+
+    // Approach 2: array and hashing
+    const result = {};
+    let flag = false;
+
+    for (let num of nums) {
+        result[num] ? result[num]++ : result[num] = 1;
+
+        if (result[num] > 1) {
+            flag = true;
+            break;
+        }
+    }
+
+    return flag;
 };
