f30b765999ad87a1961d6e7ee126dd4595eb4a4f
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-12-03 17:53:11 +0200

    [242] [Time Beats: 80.77%] [Memory Beats: 60.04%] - LeetPush

--- 242-Valid-Anagram.js
+++ 242-Valid-Anagram.js
@@ -12,20 +12,34 @@ var isAnagram = function (s, t) {
     // return s === t;
 
     // Approch 2: hashing
-    const result = {};
+    // const result = {};
 
-    for (const letter of s) {
-        result[letter] ? result[letter]++ : result[letter] = 1;
+    // for (const letter of s) {
+    //     result[letter] ? result[letter]++ : result[letter] = 1;
+    // }
+
+    // for (const letter of t) {
+    //     result[letter] ? result[letter]-- : result[letter] = 1;
+    // }
+
+    // let valueSum = 0;
+    // for (const key in result) {
+    //     valueSum += result[key];
+    // }
+
+    // return valueSum === 0;
+
+    // Approach 3: hashing using Map()
+    const charCount = new Map();
+
+    for (const char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1)
     }
 
-    for (const letter of t) {
-        result[letter] ? result[letter]-- : result[letter] = 1;
-    }
-    
-    let valueSum = 0;
-    for (const key in result) {
-        valueSum += result[key];
+    for (const char of t) {
+        if (!charCount.has(char) || charCount.get(char) === 0) return false;
+        charCount.set(char, charCount.get(char) - 1);
     }
 
-    return valueSum === 0;
+    return true;
 };
