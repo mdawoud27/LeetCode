db432310112a58acb24757ec34063fffddfc71fc
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-12-08 23:45:44 +0200

    [14] [Time Beats: 100.00%] [Memory Beats: 80.70%] - LeetPush

--- 14-Longest-Common-Prefix.js
+++ 14-Longest-Common-Prefix.js
@@ -5,17 +5,30 @@
 var longestCommonPrefix = function (strs) {
     if (strs.length === 0) return '';
 
-    let prefix = strs.reduce((a, b) => (a <= b ? a : b));
+    // Approach 1: 
+    // let prefix = strs.reduce((a, b) => (a <= b ? a : b));
 
-    for (let i = 0; i < prefix.length; ++i) {
-        const char = prefix[i];
+    // for (let i = 0; i < prefix.length; ++i) {
+    //     const char = prefix[i];
 
-        for (const word of strs) {
-            if (word[i] !== char) {
-                return prefix.slice(0, i);
-            }
+    //     for (const word of strs) {
+    //         if (word[i] !== char) {
+    //             return prefix.slice(0, i);
+    //         }
+    //     }
+    // }
+
+    // return prefix;
+
+    // Approach 2:
+    let prefix = strs[0];
+
+    for (const word of strs) {
+        while (word.indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+
+            if (prefix === '') return '';
         }
     }
-
-    return prefix;
+    return prefix
 };
