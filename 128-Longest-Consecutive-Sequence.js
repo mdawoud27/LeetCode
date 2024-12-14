38375d9c736c23ba6aa68c1333e4790e30cb905c
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-12-14 16:43:55 +0200

    [128] [Time Beats: 95.95%] [Memory Beats: 22.84%] - LeetPush

--- 128-Longest-Consecutive-Sequence.js
+++ 128-Longest-Consecutive-Sequence.js
@@ -4,19 +4,38 @@
  */
 var longestConsecutive = function (nums) {
     if (!nums.length) return 0;
-    
-    nums.sort((a, b) => a - b);
 
-    let counter = 1, maxLength = 1;
+    // Approach 1: use sorting and tracking
+    // nums.sort((a, b) => a - b);
 
-    for (let i = 1; i < nums.length; ++i) {
-        if (nums[i] === nums[i - 1]) continue;
-        else if (nums[i] - nums[i - 1] === 1) {
-            counter += 1;
+    // let counter = 1, maxLength = 1;
+
+    // for (let i = 1; i < nums.length; ++i) {
+    //     if (nums[i] === nums[i - 1]) continue;
+    //     else if (nums[i] - nums[i - 1] === 1) {
+    //         counter += 1;
+    //         maxLength = Math.max(counter, maxLength);
+    //     } else {
+    //         counter = 1;
+    //     }
+    // }
+
+    // Approach 2: use hash-set
+    const numSet = new Set(nums);
+    let maxLength = 0;
+
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let counter = 1;
+            
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                counter += 1;
+            }
             maxLength = Math.max(counter, maxLength);
-        } else {
-            counter = 1;
         }
     }
+
     return maxLength;
 };
