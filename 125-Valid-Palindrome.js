bb0f04195119fd5a3f59da225f0b7e843220b0d1
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2024-12-16 19:32:06 +0200

    [125] [Time Beats: 85.70%] [Memory Beats: 41.34%] - LeetPush

--- 125-Valid-Palindrome.js
+++ 125-Valid-Palindrome.js
@@ -3,19 +3,22 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    s = s.toLowerCase().trim();
+    // s = s.toLowerCase().trim();
 
-    let temp = '';
+    // let temp = '';
 
-    for (let i = 0; i < s.length; ++i) {
-        if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9') temp += s[i];
-    }
+    // for (let i = 0; i < s.length; ++i) {
+    //     if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9') temp += s[i];
+    // }
 
-    let i = 0, j = temp.length - 1;
-    while (i <= j) {
-        if (temp[i] !== temp[j]) return false;
-        i++;
-        j--;
-    }
-    return true;
+    // let i = 0, j = temp.length - 1;
+    // while (i <= j) {
+    //     if (temp[i] !== temp[j]) return false;
+    //     i++;
+    //     j--;
+    // }
+    // return true;
+
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return str.split('').reverse().join('') === str; 
 };
