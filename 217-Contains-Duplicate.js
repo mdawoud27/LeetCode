07f6cb2bd437fc2b26523567839c26321e6df38c
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-01-22 09:22:18 +0200

    [217] [Time Beats: 83.08%] [Memory Beats: 5.19%] - LeetPush

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -3,39 +3,17 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    /** 
-    // Approach 1: convert array to set and compare between their size
-
-    const setNums = new Set(nums);
-    return nums.length !== setNums.size;
-    */
-
-    /**
-    // Approach 2: array and hashing
-    const result = {};
+    const result = new Map();
     let flag = false;
 
-    for (let num of nums) {
-        result[num] ? result[num]++ : result[num] = 1;
+    for (const num of nums) {
+        result.has(num) ? result.set(num, result.get(num) + 1) : result.set(num, 1);
 
-        if (result[num] > 1) {
+        if (result.get(num) > 1) {
             flag = true;
             break;
         }
     }
 
-    return flag;
-    */
-
-    // Approach 3: sort the array and compare between their elements
-    nums.sort((a, b) => a - b);
-    let flag = false;
-
-    for (let i = 1; i < nums.length; ++i) {
-        if (nums[i] === nums[i - 1]) {
-            flag = true;
-            break;
-        }
-    }
     return flag;
 };
