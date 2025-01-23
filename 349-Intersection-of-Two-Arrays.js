b112da47254123efd06cceb13930786b450b119a
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-01-23 13:48:56 +0200

    [349] [Time Beats: 63.10%] [Memory Beats: 40.21%] - LeetPush

--- 349-Intersection-of-Two-Arrays.js
+++ 349-Intersection-of-Two-Arrays.js
@@ -4,8 +4,8 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-    const nums1Set = new Set(nums1);
-    const nums2Set = new Set(nums2);
+    const nums1Set = new Set(nums1); // O(n) - space
+    const nums2Set = new Set(nums2); // O(m) - space
 
-    return [...new Set([...nums1Set].filter(num => nums2Set.has(num)))];
+    return [...new Set([...nums1Set].filter(num => nums2Set.has(num)))]; // O(n + m) - time
 };
