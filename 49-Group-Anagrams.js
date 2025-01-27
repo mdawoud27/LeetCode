0e1bf9508b2122f39f4298eb900cac19af07cb1b
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-01-27 13:03:16 +0200

    [49] [Time Beats: 95.27%] [Memory Beats: 49.24%] - LeetPush

--- 49-Group-Anagrams.js
+++ 49-Group-Anagrams.js
@@ -5,31 +5,18 @@
 var groupAnagrams = function (strs) {
     const result = new Map();
 
-    /* Approach 1: hashing with sorting */
-    // for (const word of strs) {
-    //     const sortedWord = word.split('').sort().join('');
-
-    //     if (result.has(sortedWord)) {
-    //         result.get(sortedWord).push(word)
-    //     } else {
-    //         result.set(sortedWord, [word]);
-    //     }
-    // }
-
-    /* Approach 2: hashing with freq. array */
     for (const word of strs) {
         const freq = new Array(26).fill(0);
-
-        for (const char of word) {
-            freq[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        for (let i = 0; i < word.length; ++i) {
+            freq[word[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
         }
-        const key = freq.join('#');
 
+        const key = freq.join('#');
         if (!result.has(key)) {
             result.set(key, []);
         }
+
         result.get(key).push(word);
     }
-
-    return Array.from(result.values());
+    return [...result.values()];
 };
