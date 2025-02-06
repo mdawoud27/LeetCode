d6046e8711b88cf27afd146650181723e74c44bf
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-02-06 12:00:10 +0200

    [412] [Time Beats: 51.72%] [Memory Beats: 10.45%] - LeetPush

--- 412-Fizz-Buzz.js
+++ 412-Fizz-Buzz.js
@@ -5,8 +5,10 @@
 var fizzBuzz = function (n) {
     const result = [];
 
+    if (n < 1) return result;
+
     for (let i = 1; i <= n; ++i) {
-        if (i % 3 === 0 && i % 5 === 0) result.push('FizzBuzz');
+        if (i % 15 === 0) result.push('FizzBuzz');
         else if (i % 3 === 0) result.push('Fizz');
         else if (i % 5 === 0) result.push('Buzz');
         else result.push(i.toString());
