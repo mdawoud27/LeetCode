d4b98ff4cae407a8f3ab6a40b2775f9a92f92941
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-02-06 12:08:49 +0200

    [58] [Time Beats: 100.00%] [Memory Beats: 21.81%] - LeetPush

--- 58-Length-of-Last-Word.js
+++ 58-Length-of-Last-Word.js
@@ -3,23 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    // Approach 1
-    // return s.trim().split(' ').pop().length;
-
-    // Approach 2: The optimize solution
-    let length = 0;
-    let i = s.length - 1;
-
-    // Remove the spaces from the end if found
-    while (i >= 0 && s[i] === ' ') {
-        i--;
-    }
-
-    // Count the last word
-    while (i >= 0 && s[i] !== ' ') {
-        length++;
-        i--;
-    }
-
-    return length;
+    const splitedStr = s.trim().split(' ');
+    return splitedStr[splitedStr.length - 1].length;
 };
