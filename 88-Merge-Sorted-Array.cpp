c3858b21b905ccf91047ae63b510cb9fd8a1d1c9
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-02-06 13:20:43 +0200

    [88] [Time Beats: 100.00%] [Memory Beats: 9.60%] - LeetPush

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -7,11 +7,14 @@ public:
         while (i < m && j < n) {
             if (nums1[i] <= nums2[j])
                 sortedArray.push_back(nums1[i]), i++;
-            else sortedArray.push_back(nums2[j]), j++;
+            else
+                sortedArray.push_back(nums2[j]), j++;
         }
 
-        while (i < m) sortedArray.push_back(nums1[i++]);
-        while (j < n) sortedArray.push_back(nums2[j++]);
+        while (i < m)
+            sortedArray.push_back(nums1[i++]);
+        while (j < n)
+            sortedArray.push_back(nums2[j++]);
 
         nums1 = sortedArray;
     }
