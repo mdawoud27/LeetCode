9fa810b889198146a5da7ac4183ac6f5c1acfcd7
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-02-06 12:47:12 +0200

    [771] [Time Beats: 100.00%] [Memory Beats: 91.65%] - LeetPush

--- 771-Jewels-and-Stones.js
+++ 771-Jewels-and-Stones.js
@@ -6,11 +6,11 @@
 var numJewelsInStones = function (jewels, stones) {
     let nStons = 0;
 
-    for (let i = 0; i < jewels.length; ++i) {
-        for (let j = 0; j < stones.length; ++j) {
-            if (jewels[i] === stones[j]) nStons++;
-        }
+
+    for (let stone of stones) {
+        if (jewels.indexOf(stone) != -1) nStons++;
     }
+
 
     return nStons;
 };
