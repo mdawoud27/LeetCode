a403f5d814e117b927db4e5a939f1a52ccd985f0
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-02-17 11:46:19 +0200

    [242] [Time Beats: 57.91%] [Memory Beats: 11.88%] - LeetPush

--- 242-Valid-Anagram.js
+++ 242-Valid-Anagram.js
@@ -5,41 +5,17 @@
  */
 var isAnagram = function (s, t) {
     if (s.length !== t.length) return false;
-
-    // Aproach 1: sort and compare
-    // s = s.split("").sort().join("");
-    // t = t.split("").sort().join("");
-    // return s === t;
-
-    // Approch 2: hashing
-    // const result = {};
-
-    // for (const letter of s) {
-    //     result[letter] ? result[letter]++ : result[letter] = 1;
-    // }
-
-    // for (const letter of t) {
-    //     result[letter] ? result[letter]-- : result[letter] = 1;
-    // }
-
-    // let valueSum = 0;
-    // for (const key in result) {
-    //     valueSum += result[key];
-    // }
-
-    // return valueSum === 0;
-
-    // Approach 3: hashing using Map()
-    const charCount = new Map();
-
+    
+    const freqCounter = {};
     for (const char of s) {
-        charCount.set(char, (charCount.get(char) || 0) + 1)
+        freqCounter[char] = (freqCounter[char] || 0) + 1;
     }
 
     for (const char of t) {
-        if (!charCount.has(char) || charCount.get(char) === 0) return false;
-        charCount.set(char, charCount.get(char) - 1);
+        if (!freqCounter[char]) return false;
+        freqCounter[char] -= 1;
     }
-
     return true;
+
+    
 };
