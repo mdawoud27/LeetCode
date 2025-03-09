00ebb935ba2ced33f54e466581eb5ddff9ca40ce
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-03-09 19:00:55 +0200

    [55] [Time Beats: 65.44%] [Memory Beats: 30.90%] - LeetPush

--- Jump-Game.js
+++ Jump-Game.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
+var canJump = function (nums) {
     let maxReach = 0;
-    
+
     for (let i = 0; i < nums.length; i++) {
         if (i > maxReach) return false;
         maxReach = Math.max(maxReach, i + nums[i]);
         if (maxReach >= nums.length - 1) return true;
     }
-    
+
     return false;
 };
