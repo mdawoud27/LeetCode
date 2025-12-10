6e333bb5c05ac52ecea0aaab77fe78d45e00458e
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-12-10 15:16:03 +0200

    [1662] [Time Beats: 100.00%] [Memory Beats: 20.97%] - LeetPush

--- Check-If-Two-String-Arrays-are-Equivalent.ts
+++ Check-If-Two-String-Arrays-are-Equivalent.ts
@@ -1,3 +1,7 @@
 1function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
 2    return word1.join('') === word2.join('');
 3};
+4/**
+5    time: O(n1 + n2)
+6    space: O(n1 + n2)
+7 */
