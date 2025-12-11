014d0b9342f519d3bba4ebdfbb7d5c4ec976e3ca
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2025-12-11 16:35:49 +0200

    [1518] [Time Beats: 100.00%] [Memory Beats: 82.89%] - LeetPush

--- Water-Bottles.ts
+++ Water-Bottles.ts
@@ -1,11 +1,10 @@
 1function numWaterBottles(numBottles: number, numExchange: number): number {
 2    let totalDrank = numBottles;
-3    let empty = numBottles;
-4
-5    while (empty >= numExchange) {
-6        const newFull = Math.floor(empty / numExchange);
-7        totalDrank += newFull;
-8        empty = newFull + empty % numExchange
-9    }
-10    return totalDrank;
-11};
+3
+4    while (numBottles >= numExchange) {
+5        const newFull = Math.floor(numBottles / numExchange);
+6        totalDrank += newFull;
+7        numBottles = newFull + numBottles % numExchange
+8    }
+9    return totalDrank;
+10};
