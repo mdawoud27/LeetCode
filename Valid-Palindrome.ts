3680b9aa6ad67849e9c8066ebafeefa3dbc72303
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-01-29 14:08:53 +0200

    [125] [Time Beats: 100.00%] [Memory Beats: 57.19%] - LeetPush

--- Valid-Palindrome.ts
+++ Valid-Palindrome.ts
@@ -1,17 +1,16 @@
 1function isPalindrome(s: string): boolean {
-2    let i = 0;
-3    let j = s.length - 1;
+2    let l: number = 0;
+3    let r: number = s.length - 1;
 4
 5    const isAlphaNum = (c: string) => c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9';
 6
-7    while (i < j) {
-8        while (i < j && !isAlphaNum(s[i])) i++;
-9        while (i < j && !isAlphaNum(s[j])) j--;
+7    while (l < r) {
+8        while (l < r && !isAlphaNum(s[l])) l++; 
+9        while (l < r && !isAlphaNum(s[r])) r--; 
 10
-11        if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
-12
-13        i++;
-14        j--;
-15    }
-16    return true;
-17};
+11        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+12        l++;
+13        r--;
+14    }
+15    return true;
+16};
