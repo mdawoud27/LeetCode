c03b2aafb63230ba0757e1456322cdb8f0fbe350
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-08 20:35:58 +0300

    [121] [Time Beats: 77.23%] [Memory Beats: 36.19%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.ts
+++ Best-Time-to-Buy-and-Sell-Stock.ts
@@ -1,16 +1,33 @@
 1function maxProfit(prices: number[]): number {
-2    let buyPrice: number = prices[0];
-3    let maxProfit: number = 0;
-4
-5    for (let i = 1; i < prices.length; i++) {
-6        if (prices[i] < buyPrice) {
-7            buyPrice = prices[i];
-8        } else {
-9            const profit = prices[i] - buyPrice;
-10            if (profit > maxProfit) {
-11                maxProfit = profit;
-12            }
-13        }
-14    }
-15    return maxProfit;
-16};
+2    // approach 1
+3    // let buyPrice: number = prices[0];
+4    // let maxProfit: number = 0;
+5
+6    // for (let i = 1; i < prices.length; i++) {
+7    //     if (prices[i] < buyPrice) {
+8    //         buyPrice = prices[i];
+9    //     } else {
+10    //         const profit = prices[i] - buyPrice;
+11    //         if (profit > maxProfit) {
+12    //             maxProfit = profit;
+13    //         }
+14    //     }
+15    // }
+16
+17    // approach 2 => sliding window
+18    let left = 0; // for buy
+19    let right = 1; // for sell
+20    let maxProfit = 0;
+21
+22    while (right < prices.length) {
+23        if (prices[right] > prices[left]) {
+24            const profit = prices[right] - prices[left];
+25            maxProfit = Math.max(maxProfit, profit);
+26        } else {
+27            left = right;
+28        }
+29        right++;
+30    }
+31
+32    return maxProfit;
+33};
