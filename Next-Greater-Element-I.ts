70da5d5b01f7e12234e31c5e6ccf95b342ea5013
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-11 10:46:20 +0300

    [496] [Time Beats: 45.58%] [Memory Beats: 37.80%] - LeetPush

--- Next-Greater-Element-I.ts
+++ Next-Greater-Element-I.ts
@@ -1,17 +1,34 @@
 1function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-2    const ans: number[] = [];
-3
-4    for (const n of nums1) {
-5        const index = nums2.indexOf(n);
-6
-7        let nextGreater = -1;
-8        for (let i = index + 1; i < nums2.length; i++) {
-9            if (nums2[i] > n) { 
-10                nextGreater = nums2[i]; 
-11                break; 
-12            }
-13        }
-14        ans.push(nextGreater);
-15    }
-16    return ans;
-17};
+2    // Approach 1
+3    // const ans: number[] = [];
+4
+5    // for (const n of nums1) {
+6    //     const index = nums2.indexOf(n);
+7
+8    //     let nextGreater = -1;
+9    //     for (let i = index + 1; i < nums2.length; i++) {
+10    //         if (nums2[i] > n) { 
+11    //             nextGreater = nums2[i]; 
+12    //             break; 
+13    //         }
+14    //     }
+15    //     ans.push(nextGreater);
+16    // }
+17    // return ans; // O(n^2)
+18
+19    // Stack approach
+20    const stack: number[] = [];
+21    const map = new Map<number, number>();
+22
+23    for (const num of nums2) {
+24        while (stack.length && num > stack[stack.length - 1]) {
+25            map.set(stack.pop(), num);
+26        }
+27
+28        stack.push(num);
+29    }
+30
+31    while (stack.length) map.set(stack.pop(), -1);
+32
+33    return nums1.map(num => map.get(num));
+34};
