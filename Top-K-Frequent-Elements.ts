d61cfaa7a196f06948c52dda417893be6f76788b
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-12 16:05:08 +0300

    [347] [Time Beats: 57.81%] [Memory Beats: 86.30%] - LeetPush

---
+++ Top-K-Frequent-Elements.ts
@@ -0,0 +1,17 @@
+1function topKFrequent(nums: number[], k: number): number[] {
+2    const map = new Map<number, number>();
+3    const ans: number[] = [];
+4
+5    for (const num of nums) {
+6        if (map.has(num)) map.set(num, map.get(num)! + 1);
+7        else map.set(num, 1);
+8    }
+9
+10    const result = [...map.entries()].sort((a, b) => b[1] - a[1]);
+11
+12    for (const [key, val] of result) {
+13        if (k--) ans.push(key);
+14        else break;
+15    }
+16    return ans;
+17}
