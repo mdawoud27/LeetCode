6bcc8b31f1c819d4aeb0423a015a392035e8a863
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-14 17:44:06 +0300

    [49] [Time Beats: 27.04%] [Memory Beats: 73.00%] - LeetPush

--- Group-Anagrams.ts
+++ Group-Anagrams.ts
@@ -1,17 +1,31 @@
-function groupAnagrams(strs: string[]): string[][] {
-    const map = new Map<string, string[]>();
-
-    for (const str of strs) {
-        const freq = new Array(26).fill(0);
-        for (const char of str) {
-            freq[char.charCodeAt(0) - 97]++;
-        }
-
-        const key = freq.join('#');
-
-        if (!map.has(key)) map.set(key, []);
-        map.get(key).push(str);
-    }
-
-    return [...map.values()]
-};
+1function groupAnagrams(strs: string[]): string[][] {
+2    if (strs.length === 1) return [strs];
+3
+4    const map = new Map<string, string[]>();
+5    const result: string[][] = [];
+6
+7    for (const str of strs) {
+8        const code = genCodeOfStr(str);
+9
+10        if (!map.has(code)) map.set(code, []);
+11        map.get(code).push(str);
+12    }
+13
+14    for (const [key, value] of map) {
+15        result.push(value);
+16    }
+17
+18    return result;
+19};
+20
+21function genCodeOfStr(str: string): string {
+22    const freq: number[] = new Array(26).fill(0);
+23
+24    for (let i = 0; i < str.length; i++) {
+25        freq[str[i].charCodeAt(0) - 97] += 1;
+26    }
+27
+28    const strCode = freq.join('#');
+29
+30    return strCode;
+31}
