5fe3735a46890d1adf4375434898ee4465c16a74
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-14 17:31:14 +0300

    [242] [Time Beats: 90.70%] [Memory Beats: 86.31%] - LeetPush

--- Valid-Anagram.ts
+++ Valid-Anagram.ts
@@ -1,18 +1,35 @@
 1function isAnagram(s: string, t: string): boolean {
 2    if (s.length !== t.length) return false;
 3
-4    const map = new Map<string, number>();
+4    // const map = new Map<string, number>();
 5
-6    for (const char of s) {
-7        map.set(char, (map.get(char) ?? 0) + 1);
-8    }
+6    // for (const char of s) {
+7    //     map.set(char, (map.get(char) ?? 0) + 1);
+8    // }
 9
-10    for (const char of t) {
-11        map.set(char, (map.get(char) ?? -1) - 1);
-12    }
+10    // for (const char of t) {
+11    //     map.set(char, (map.get(char) ?? -1) - 1);
+12    // }
 13
-14    for (const [key, value] of map) {
-15        if (value) return false;
-16    }
-17    return true;
-18};
+14    // for (const [key, value] of map) {
+15    //     if (value) return false;
+16    // }     o(n)
+17
+18    // another approach
+19    const freq1: number[] = new Array(26).fill(0);
+20    const freq2: number[] = new Array(26).fill(0);
+21
+22    for (let i = 0; i < s.length; i++) {
+23        freq1[s[i].charCodeAt(0) - 97] += 1;
+24    }
+25    
+26    for (let i = 0; i < t.length; i++) {
+27        freq2[t[i].charCodeAt(0) - 97] += 1;
+28    }
+29
+30    const code1 = freq1.join('#');
+31    const code2 = freq2.join('#');
+32
+33
+34    return code1 === code2;
+35};
