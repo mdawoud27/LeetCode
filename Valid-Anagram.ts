5a6f6586a45e10b442556871c022f8b6d95bb3f9
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-14 17:18:28 +0300

    [242] [Time Beats: 26.79%] [Memory Beats: 5.28%] - LeetPush

--- Valid-Anagram.ts
+++ Valid-Anagram.ts
@@ -1,15 +1,18 @@
-function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) return false;
-
-    const map = new Map<string, number>();
-    for (const char of s) {
-        map.set(char, (map.get(char) || 0) + 1);
-    }
-
-    for (const char of t) {
-        if (!map.has(char)) return false;
-        map.set(char, map.get(char) - 1);
-        if (map.get(char) === 0) map.delete(char);
-    }
-    return map.size === 0;
-};
+1function isAnagram(s: string, t: string): boolean {
+2    if (s.length !== t.length) return false;
+3
+4    const map = new Map<string, number>();
+5
+6    for (const char of s) {
+7        map.set(char, (map.get(char) || 0) + 1);
+8    }
+9
+10    for (const char of t) {
+11        map.set(char, (map.get(char) || -1) - 1);
+12    }
+13
+14    for (const [key, value] of map) {
+15        if (value) return false;
+16    }
+17    return true;
+18};
