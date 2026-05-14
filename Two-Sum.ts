f9dcb3838c069c4777f3e2fb484115048cdf5666
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-14 17:06:26 +0300

    [1] [Time Beats: 100.00%] [Memory Beats: 31.38%] - LeetPush

--- Two-Sum.ts
+++ Two-Sum.ts
@@ -1,11 +1,14 @@
 1function twoSum(nums: number[], target: number): number[] {
-2    const ans = {};
+2    if (nums.length === 2) return [0, 1];
 3
-4    for (let i = 0; i < nums.length; i++) {
-5        const temp = target - nums[i];
+4    const map = new Map<number, number>();
+5    const result: number[] = [];
 6
-7        if (ans.hasOwnProperty(temp)) return [i, ans[temp]];
-8        ans[nums[i]] = i;
-9    }
-10    return [];
-11};
+7    for (let i = 0; i < nums.length; i++) {
+8        const temp = target - nums[i];
+9
+10        if (map.has(temp)) return [i, map.get(temp)];
+11        map.set(nums[i], i);
+12    }
+13    return [];
+14};
