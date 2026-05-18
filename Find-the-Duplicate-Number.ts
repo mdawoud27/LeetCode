ab040b72ddbadf46780de1219b5ce7a98f6331a7
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-05-18 12:03:58 +0300

    [287] [Time Beats: 22.34%] [Memory Beats: 21.31%] - LeetPush

--- Find-the-Duplicate-Number.ts
+++ Find-the-Duplicate-Number.ts
@@ -1,8 +1,9 @@
-function findDuplicate(nums: number[]): number {
-    const map = new Map<number, number>();
-
-    for (const num of nums) {
-        if (map.has(num)) return num;
-        map.set(num, 1);
-    }
-};
+1function findDuplicate(nums: number[]): number {
+2    const set = new Set<number>();
+3
+4    for (const num of nums) {
+5        if (set.has(num)) return num;
+6        set.add(num);
+7    }
+8    return -1;
+9};
