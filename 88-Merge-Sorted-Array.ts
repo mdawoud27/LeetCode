096f85440cdacdf99d388a01b1ce4e8da11338e3
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-05 19:06:01 +0300

    [88] [Time Beats: 100.00%] [Memory Beats: 82.54%] - LeetPush

---
+++ 88-Merge-Sorted-Array.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1, j = n - 1, k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+};
