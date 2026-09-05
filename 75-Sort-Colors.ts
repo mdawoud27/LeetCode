3b840112de3d959d3ba1800d6c5ba3000873a5e9
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-05 19:54:44 +0300

    [75] [Time Beats: 46.79%] [Memory Beats: 90.12%] - LeetPush

---
+++ 75-Sort-Colors.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+    let low = 0, mid = 0, high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]]
+            high--;
+        }
+    }
+};
