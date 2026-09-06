2a75e50cbc5a52e1962af3b2c0f9aab5bbe936b7
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-06 10:01:28 +0300

    [739] [Time Beats: 95.40%] [Memory Beats: 10.24%] - LeetPush

---
+++ 739-Daily-Temperatures.ts
@@ -0,0 +1,17 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    const n = temperatures.length;
+    const answer = new Array(n).fill(0);
+    const stack: number[] = [];
+
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length > 0 && temperatures[stack[stack.length - 1]] <= temperatures[i]) {
+            stack.pop();
+        }
+
+        if (stack.length > 0) {
+            answer[i] = stack[stack.length - 1] - i;
+        }
+        stack.push(i);
+    }
+    return answer;
+}
