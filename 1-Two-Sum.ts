48c1f317202c99a8f4825bfae8fb5070ec9ade60
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-08 09:59:16 +0300

    [1] [Time Beats: 61.91%] [Memory Beats: 22.91%] - LeetPush

---
+++ 1-Two-Sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    const seen = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (seen.has(complement)) {
+            return [i, seen.get(complement)];
+        }
+        seen.set(nums[i], i);
+    }
+
+    return [-1, -1];
+};
