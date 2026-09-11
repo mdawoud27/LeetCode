ca43d91c692e4ac5669d6b119d3f89741f085628
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-11 11:45:40 +0300

    [383] [Time Beats: 90.44%] [Memory Beats: 27.77%] - LeetPush

--- 383-Ransom-Note.ts
+++ 383-Ransom-Note.ts
@@ -1,12 +1,15 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    const map = new Map<string, number>();
+    const freqCount = new Array(26).fill(0);
 
-    for (const c of ransomNote) map.set(c, (map.get(c) ?? 0) + 1);
     for (const c of magazine) {
-        if (map.has(c) && map.get(c) > 0) {
-            map.set(c, (map.get(c) ?? 0) - 1);
-        }
+        const idx = c.charCodeAt(0) - 'a'.charCodeAt(0);
+        freqCount[idx]++;
     }
 
-    return [...map.values()].every(v => v === 0);
+    for (const c of ransomNote) {
+        const idx = c.charCodeAt(0) - 'a'.charCodeAt(0);
+        freqCount[idx]--;
+        if (freqCount[idx] < 0) return false;
+    }
+    return true;
 };
