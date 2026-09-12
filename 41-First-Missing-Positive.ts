58724bc57fa32240ce28cfb42f325f32e2c32274
Mohamed Dawoud <113205913+mdawoud27@users.noreply.github.com>
2026-09-12 12:34:27 +0300

    [41] [Time Beats: 98.85%] [Memory Beats: 96.92%] - LeetPush

--- 41-First-Missing-Positive.ts
+++ 41-First-Missing-Positive.ts
@@ -1,7 +1,19 @@
 function firstMissingPositive(nums: number[]): number {
-    const set = new Set<number>([...nums]);
+    const n = nums.length;
 
-    for (let i = 1; ; i++) {
-        if (!set.has(i)) return i;
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n) {
+            const targetIdx = nums[i] - 1;
+
+            if (nums[targetIdx] === nums[i]) break;
+
+            [nums[i], nums[targetIdx]] = [nums[targetIdx], nums[i]];
+        }
     }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) return i + 1;
+    }
+
+    return n + 1;
 };
